e0f92cc0127fc3a9afde827e244851f7dc44712d
Milin555 <dholiyamilin@gmail.com>
2026-02-12 17:45:31 +0530

    Arrowicon fix in categories

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Jeans from '../../assets/img/jeans.jpg'
+import ArrowIcon from '../common/Arrowicon'
 export const Card = ({ imagePath, title, description, actionArrow }) => {
   return (
     <div className="flex flex-col p-8">
@@ -7,12 +8,15 @@ export const Card = ({ imagePath, title, description, actionArrow }) => {
         className="h-[292px] w-[231px] object-cover transition-transform duration-300 hover:scale-105 cursor-pointer"
         src={imagePath}
         alt={title} 
-      /><div className='flex flex-col'>
+      />
+        <div className='flex justify-between items-center'>
+      <div className='flex flex-col'>
           <p className='text-[16px] p-1'>{title}</p>
           {description && <p className='text-[12px] px-1 text-gray-600'>{description}</p>}
           </div>
-          {actionArrow && <div>
-            </div>}
+        
+          {actionArrow && <span className='cursor-pointer pr-2 items-center'><ArrowIcon /></span>}
+            </div>
 
     </div>
   )
